0fc27af2b8c4f05c94d64d9de856a417e71bac5a
mvantaggi <mattiavantaggi@gmail.com>
2023-12-20 21:12:47 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,202 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:random_color/random_color.dart'; // Il pacchetto per generare colori casuali
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class VotiPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'VotoTracker'),
-    );
-  }
+  _VotiPageState createState() => _VotiPageState();
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class VotoData {
+  String nome; // Il nome del candidato
+  int voti; // Il numero dei voti ricevuti
+  charts.Color barColor; // Il colore della barra nel grafico
+  VotoData({required this.nome, required this.voti, this.barColor});
 }
+class _VotiPageState extends State<VotiPage> {
+  // La classe che rappresenta i dati del grafico
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // La lista dei dati del grafico
+  List<VotoData> dati = [
+  VotoData(nome: "Alice", voti: 10, barColor: charts.ColorUtil.fromDartColor(Colors.blue)),
+  VotoData(nome: "Bob", voti: 15, barColor: charts.ColorUtil.fromDartColor(Colors.red)),
+  VotoData(nome: "Carlo", voti: 20, barColor: charts.ColorUtil.fromDartColor(Colors.green)),
+  VotoData(nome: "Schede bianche", voti: 5, barColor: charts.ColorUtil.fromDartColor(Colors.grey)), // L'elemento per le schede bianche
+  VotoData(nome: "Schede nulle", voti: 3, barColor: charts.ColorUtil.fromDartColor(Colors.grey)), // L'elemento per le schede nulle
+  ];
+
+  // La variabile che tiene traccia del vincitore
+  String vincitore ="";
+
+  // Il controller per il widget TextField
+  TextEditingController controller = TextEditingController();
+
+  // Il metodo che crea il widget del grafico
+  Widget creaGrafico() {
+  // La serie di dati da visualizzare nel grafico
+  List<charts.Series<VotoData, String>> serie = [
+  charts.Series(
+  id: "Voti",
+  data: dati,
+  domainFn: (VotoData voto, _) => voto.nome, // La funzione che restituisce il nome del candidato
+  measureFn: (VotoData voto, _) => voto.voti, // La funzione che restituisce il numero dei voti
+  colorFn: (VotoData voto, _) => voto.barColor, // La funzione che restituisce il colore della barra
+  labelAccessorFn: (VotoData voto, _) => '${voto.voti}', // La funzione che restituisce il numero dei voti da mostrare sulle barre
+  )
+  ];
+
+  // Il widget del grafico a barre
+  return charts.BarChart(
+  serie,
+  animate: true, // Abilita le animazioni
+  vertical: false, // Rende il grafico orizzontale
+  barRendererDecorator: charts.BarLabelDecorator<String>(), // Mostra le etichette sulle barre
+  domainAxis: charts.OrdinalAxisSpec(renderSpec: charts.NoneRenderSpec()), // Nasconde le etichette sull'asse X
+  behaviors: [
+  charts.ChartTitle('Grafico dei voti', // Il titolo del grafico
+  behaviorPosition: charts.BehaviorPosition.top, // La posizione del titolo
+  titleOutsideJustification: charts.OutsideJustification.start, // L'allineamento del titolo
+  innerPadding: 18), // Il padding del titolo
+  charts.Legend(), // La legenda del grafico
+  ],
+  );
   }
 
+  // Il metodo che crea il widget del bottone
+  Widget creaBottone(String nome, int index, bool incrementa) {
+  // Il widget del bottone
+  return ElevatedButton(
+  child: Text(incrementa ? "+" : "-"), // Il testo del bottone
+  onPressed: () {
+  // La logica da eseguire quando il bottone viene premuto
+  setState(() {
+  // Modifica il valore dei voti in base al parametro incrementa
+  if (incrementa) {
+  dati[index].voti++;
+  } else {
+  dati[index].voti--;
+  }
+  // Ordina la lista dei dati in base al numero dei voti
+  dati.sort((a, b) => b.voti.compareTo(a.voti));
+  // Calcola il numero totale dei votanti
+  int totale = dati.fold(0, (sum, voto) => sum + voto.voti);
+  // Calcola la maggioranza dei voti
+  int maggioranza = (totale / 2).ceil();
+  // Controlla se il primo candidato ha raggiunto la maggioranza
+  if (dati[0].voti >= maggioranza) {
+  // Imposta il vincitore con il nome del primo candidato
+  vincitore = dati[0].nome;
+  } else {
+  // Azzera il vincitore
+  vincitore = "";
+  }
+  });
+  },
+  );
+  }
+
+  // Il metodo che crea il widget del TextField
+  Widget creaTextField() {
+  // Il widget del TextField
+  return TextField(
+  controller: controller, // Il controller del TextField
+  keyboardType: TextInputType.number, // La tipologia di tastiera da mostrare
+  decoration: InputDecoration(
+  labelText: "Inserisci il numero dei candidati", // L'etichetta del TextField
+  border: OutlineInputBorder(), // Il bordo del TextField
+  ),
+  onSubmitted: (value) {
+  // La logica da eseguire quando il valore viene inviato
+  setState(() {
+  // Converte il valore in un numero intero
+  int numero = int.tryParse(value) ?? 0;
+  // Controlla se il numero è valido
+  if (numero > 0) {
+  // Crea un generatore di colori casuali
+  RandomColor randomColor = RandomColor();
+  // Svuota la lista dei dati
+  dati.clear();
+  // Aggiunge il numero di candidati richiesto alla lista dei dati
+  for (int i = 0; i < numero; i++) {
+  // Crea un nome casuale per il candidato
+  String nome = "Candidato ${i + 1}";
+  // Crea un numero casuale di voti per il candidato
+  int voti = (10 + i) * (i + 1);
+  // Crea un colore casuale per il candidato
+  charts.Color barColor = charts.ColorUtil.fromDartColor(randomColor.randomColor());
+  // Crea un oggetto VotoData con i dati del candidato
+  VotoData voto = VotoData(nome: nome, voti: voti, barColor: barColor);
+  // Aggiunge l'oggetto alla lista dei dati
+  dati.add(voto);
+  }
+  // Aggiunge le schede bianche e nulle alla lista dei dati
+  dati.add(VotoData(nome: "Schede bianche", voti: 5, barColor: charts.ColorUtil.fromDartColor(Colors.grey)));
+  dati.add(VotoData(nome: "Schede nulle", voti: 3, barColor: charts.ColorUtil.fromDartColor(Colors.grey)));
+  // Ordina la lista dei dati in base al numero dei voti
+  dati.sort((a, b) => b.voti.compareTo(a.voti));
+  // Calcola il numero totale dei votanti
+  int totale = dati.fold(0, (sum, voto) => sum + voto.voti);
+  // Calcola la maggioranza dei voti
+  int maggioranza = (totale / 2).ceil();
+  // Controlla se il primo candidato ha raggiunto la maggioranza
+  if (dati[0].voti >= maggioranza) {
+  // Imposta il vincitore con il nome del primo candidato
+  vincitore = dati[0].nome;
+  } else {
+  // Azzera il vincitore
+  vincitore = "";
+  }
+  }
+  });
+  },
+  );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  // Il widget della pagina
+  return Scaffold(
+  appBar: AppBar(
+  title: Text("Grafico dei voti"),
+  ),
+  body: Column(
+  children: [
+  // Il widget del grafico
+  Expanded(
+  child: Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: creaGrafico(),
+  ),
+  ),
+  // Il widget del TextField
+  Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: creaTextField(),
+  ),
+  // Il widget dei bottoni
+  ListView.builder(
+  shrinkWrap: true,
+  itemCount: dati.length,
+  itemBuilder: (context, index) {
+  // Il widget di una riga con il nome del candidato e i bottoni per aumentare o decrescere i voti
+  return Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+  Text(dati[index].nome),
+  creaBottone(dati[index].nome, index, false),
+  creaBottone(dati[index].nome, index, true),
+  ],
+  );
+  },
+  ),
+  // Il widget del messaggio del vincitore
+  if (vincitore != null)
+  Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: Text("Il vincitore è $vincitore!"),
+  ),
+  ],
+  ),
+  );
   }
 }
